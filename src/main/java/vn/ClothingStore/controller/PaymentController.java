c0e9befe986aaab7c8eb7256de6fa3b714ed3170
NguyenVanThang2004 <thangdepzai38@gmail.com>
2025-09-16 11:09:46 +0700

    payment vnpay

---
+++ src/main/java/vn/ClothingStore/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package vn.ClothingStore.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudinary.http44.api.Response;
+
+import vn.ClothingStore.domain.request.reqVnpayDTO;
+import vn.ClothingStore.service.PaymentService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<String> createPayment(@RequestBody reqVnpayDTO paymentRequest) {
+        try {
+            String paymentUrl = paymentService.createPayment(paymentRequest);
+            return ResponseEntity.ok(paymentUrl);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Đã xảy ra lỗi khi tạo thanh toán!");
+        }
+    }
+
+    @GetMapping("/payment/return")
+    public ResponseEntity<String> returnPayment(@RequestParam("vnp_ResponseCode") String responseCode) {
+        return paymentService.handlePaymentReturn(responseCode);
+    }
+
+}
---
+++ src/main/java/vn/ClothingStore/domain/request/reqVnpayDTO.java
@@ -0,0 +1,10 @@
+package vn.ClothingStore.domain.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class reqVnpayDTO {
+    private String amount;
+}
